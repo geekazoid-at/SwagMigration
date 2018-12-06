1d69dcaef64f76f2ee9f765c125290c30cdc7cc8
Andreas Bretschneider <bretschneider.andreas@gmail.com>
2018-12-06 11:17:48 +0100

    This sql fixes the magento db problem where some images dont have a _media_gallery_value entry...

--- images_query_fixing_loremipsum_bs.sql
+++ images_query_fixing_loremipsum_bs.sql
@@ -1,5 +1,4 @@
-SELECT *
-FROM catalog_product_entity_media_gallery a
-
-NATURAL LEFT JOIN catalog_product_entity_media_gallery_value b
-WHERE b.position IS NULL
+INSERT INTO catalog_product_entity_media_gallery_value (value_id, store_id, label, position, disabled)
+SELECT value_id, 0, NULL, 0, 0 FROM catalog_product_entity_media_gallery a
+WHERE NOT EXISTS
+(SELECT * FROM catalog_product_entity_media_gallery_value b WHERE a.value_id=b.value_id)
